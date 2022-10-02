b8697e97b170d9af7d3547069e1c0c6e80abb041
demmis98 <dem9847@gmail.com>
2022-10-02 17:25:19 -0500

    des

--- 04DES/src/main/java/des/DES.java
+++ 04DES/src/main/java/des/DES.java
@@ -1,110 +1,152 @@
-package des;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package des;
 
 /**
  *
- * @author alumno
- * vamos a trabajar bajo un modo de cifrado por bloques de 64 bits
+ * @author cerna
+ * 
+ * Vamos a programar bajo un modo de cifrado por bloques de 64 bits
  * vamos a manejar una llave privada de 64 bits
- * se debe de tomar para la inizializacion de la llave 56 bits
- * exactamente tal y como nos dice la teoria porque es para poder formar 16 subllaves
- * las tablas de permutacion y cajas de sustitucion
+ * Se debe de tomar para la inicialización de la llave 56 bit exactamente
+ * tal y como nos dice la teoría porque es para poder formar 
+ * 
+ * Las tablas de permutación y cajas de sustitución 
  * se van a cargar por parte del proveedor de servicios
- * para poder hacer todo esto vamos a usar librerias
+ * Para poder hacer todo esto vamos a usar Librerias
  * crypto y security
  */
-
 import java.security.*;
-//es para poer generar las claves y subllaves
+//es para poder generar las claves y subllaves
 import javax.crypto.spec.*;
 import javax.crypto.*;
 
 import java.io.*;
 
 public class DES {
-    public static void main(String args[])throws Exception{
+    
+    public static void main(String[] args)throws Exception{
         //vamos a comprobar la entrada de un archivo o fichero para cifrar
         if(args.length!=1){
             //no hay archivos cargados
             mensajeAyuda();
             System.exit(1);
+        }
+            /*Lo primero que tenemos que hacer es cargar una instancia
+            del proveedor del tipo de cifrado, para eso es la parte de las librerias
+            */
             
-            /*lo primero que tenemos que hacer es cargar un ainstancia del proveedor del tipo de cifrado
-            ára eso está la parte de las librerias*/
+            System.out.println("1.-Generar la clave DES:");
             
-            System.out.println("1.generar la clave DES: ");
-            
-            //vamos a usar la clase KeyGenerator
-            KeyGenerator generadorDes = KeyGenerator.getInstance("DES");
-            //inicializamo la llave
-            generadorDes.init(56);
+            //vamos a ocupar la clase KeyGenerator
+            KeyGenerator generadorDES = KeyGenerator.getInstance("DES");
+            //inicializamos la llave
             
             //vamos a generar las claves
-            SecretKey clave = generadorDes.generateKey();
+            SecretKey clave=generadorDES.generateKey();
             //estamos creando las 16 subllaves
             
-            System.out.println("la clave es: "+clave);
+            System.out.println("La clave es: "+clave);
             
-            //recordemos que en la criptografía moderna solo se ocupan bits o bytes
+            /*
+            Recordamos que en la criptografia moderna, solo se ocupan bits o bytes
+            */
             
-            //la llave que se ha generado no tiene formato
+            //la llave que se a generado no tiene formato
             mostrarBytes(clave.getEncoded());
             
             System.out.println("");
             
-            /*vamos a cifrar, para ello debemos de aplicar los estandares vistos en clase PKCS
-            para ello tenemos que crear una instancia del algoritmo DES en el modo de cifrado
+            /*
+            Vamos a cifrar, para ello debemos de aplicar los
+            estandares vistos en clase PKCS para ello tenemos
+            que crear una instancia del algoritmo DES en el
+            modo de cifrado
             
             ALGORITMO DES
-            MODO ECB(ELECTRONIC CODE BOOK)
+            MODO ECB (Electronic Code Book)
             RELLENO PKCS5
             */
             
-            System.out.println("2.cifrar con DES y el archivo :"+args[0]
-                    +" , dejar el resultado en: "+args[0]+".cifrado");
+            System.out.println("2.- Cifrar con DES y el archivo: "+args[0]+"dejar el resultado en: "+args[0]+".cifrado");
             
             //instancia
-            Cipher cifrado = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            Cipher cifrado=Cipher.getInstance("DES/ECB/PKCS5Padding");
             
-            cifrado.init(Cipher.ENCRYPT_MODE,clave);
+            cifrado.init(Cipher.ENCRYPT_MODE, clave);
             
-            //el problema es como vamos a leer los bloques del mensaje
-            byte[] buffer = new byte[1000];
+            //El problema es como vamos a leer los bloques del mensaje
+            
+            byte[] buffer=new byte[1000];
             byte[] bufferCifrado;
             
             //generamos los archivos
             
-            FileInputStream in = new FileInputStream(args[0]);
-            FileOutputStream out = new FileOutputStream(args[0]+".cifrado");
+            FileInputStream in= new FileInputStream(args[0]);
+            FileOutputStream out= new FileOutputStream(args[0]);
             
             //lectura
-            int bytesLeidos = in.read(buffer, 0, 1000);
+            int bytesleidos=in.read(buffer,0,1000);
             //mientras no esté al final del archivo
-            while(bytesLeidos != -1){
-                //pasar el texto claro leido al cifrador
-                bufferCifrado=cifrado.update(buffer, 0, bytesLeidos);
-                //vanos a generar la salida
+            while(bytesleidos!=-1){
+                //pasar el texto claro leído al cifrador
+                bufferCifrado=cifrado.update(buffer,0,bytesleidos);
+                //generar la salida
                 out.write(bufferCifrado);
-                bytesLeidos = in.read(buffer, 0, 1000);
+                bytesleidos=in.read(buffer,0,1000);
+                
             }
-            //vamos a reunir todos los bloques
+            //Vamos a reunir todos los bloques
             bufferCifrado=cifrado.doFinal();
             out.write(bufferCifrado);
             
             in.close();
             out.close();
-        }
-    }
-
-    private static void mensajeAyuda() {
+            
+            //vamos a descifrar
+            System.out.println("3.- Descifrar con DES el archivo:"+args[0]+".cifrado" + " , vamos a ver el resultado en el archivo: " + args[0] +".descifrado");
+            
+            //empezamos con el modo de descifrar
+            
+            cifrado.init(Cipher.DECRYPT_MODE, clave);
+            
+            //Buffer para la entrada y salida de los bits correspondientes
+            
+            byte[] bufferPlano;
+            
+            in = new FileInputStream(args[0]+".cifrado");
+            out = new FileOutputStream(args[0]+".descifrado");
+            
+            //Damos lectura de cada elemento
+            
+            bytesleidos = in.read(buffer,0,1000);
+            //Mientras no esté al final del archivo, que continue
+            while(bytesleidos !=-1){
+                //pasamos el texto plano a cifrado
+                bufferPlano=cifrado.update(buffer, 0, bytesleidos);
+                //genermaos la salida
+                out.write(bufferPlano);
+                bytesleidos=in.read(buffer,0,1000);
+            }
+            //vamos a reunir los bloques
+            bufferPlano=cifrado.doFinal();
+            
+            in.close();
+            out.close();
         
     }
 
-    private static void mostrarBytes(byte[] encoded) {
+    private static void mensajeAyuda() {
+        System.out.println("Ejemplo de un cifrado DES para archivos .txt");
+        System.out.println("Cuidado con la llave, solo debe ser de 8 caracteres");
+        System.out.println("Compruebe que si cargo el fichero o archivo para cifrar, sino no se puede");
+        System.out.println("Ñam Ñam :3");
     }
+
+    private static void mostrarBytes(byte[] buffer) {
+        System.out.write(buffer,0,buffer.length);
+    } 
 }
